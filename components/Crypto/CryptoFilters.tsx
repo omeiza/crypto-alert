a8d5a6ad949a070da940fb15fd4c4cc89cbabe78
Omeiza Owuda <owudaomeiza@gmail.com>
2022-02-22 23:37:55 +0100

    UI dev complete

--- components/Crypto/CryptoFilters.tsx
+++ components/Crypto/CryptoFilters.tsx
@@ -1,17 +1,63 @@
-const CryptoFilters = () => {
-    return (
-        <div className="filterSection">
-            <div className="inputStyle searchSection">
-                <input placeholder="Search Crypto e.g BTC" />
+import { Component } from "react";
+import { countryList } from "../Helpers";
+
+interface filterState {
+    countryCurrency: object,
+    selectedCurrency: string
+}
+
+class CryptoFilters extends Component<any, filterState> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            countryCurrency: {},
+            selectedCurrency: ''
+        };
+    }
+
+    async getFilterData(): Promise<Object[]> {
+        return await countryList();
+    }
+
+    componentDidMount() {
+        this.getFilterData()
+            .then(( currencies) => {
+                this.setState({
+                    countryCurrency: currencies[0]
+                });
+            })
+
+            .catch(err => {
+                console.log(`Error listing currencies: ${err}`)
+            })
+    }
+
+    currencyOptions(optionsObj: any) {
+        let optionsContent = [];
+        for (const coin in optionsObj) {
+            optionsContent.push (
+                <option key={coin} value={coin}>{`${coin}: ${optionsObj[coin]}`}</option>
+            );
+        }
+
+        return optionsContent;
+    }
+
+    render() {
+        return (
+            <div className="filterSection">
+                <div className="inputStyle searchSection">
+                    <input placeholder="Search Crypto e.g BTC" />
+                </div>
+                <div className="inputStyle currencySelector">
+                    <select>
+                        { this.currencyOptions(this.state.countryCurrency) }
+                    </select>
+                </div>
             </div>
-            <div className="inputStyle currencySelector">
-                <select>
-                    <option value="USD">USD</option>
-                    <option value="NGN">NGN</option>
-                </select>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default CryptoFilters;
--- components/Crypto/index.tsx
+++ components/Crypto/index.tsx
@@ -1,62 +1,44 @@
 import { Component } from 'react';
 import CryptoContent from './CryptoContent';
 import CryptoFilters from "./CryptoFilters";
+import { initialCoinList } from "../Helpers";
 
 interface cryptoProps {}
-interface cryptoState {
-    crypto_result: any
-}
+interface cryptoState { cryptoResult: any }
 
 class Crypto extends Component<cryptoProps, cryptoState> {
     constructor(props: any) {
         super(props);
 
         this.state = {
-            crypto_result: null
+            cryptoResult: null
         };
     }
 
     componentDidMount() {
-        this.getCrytpoData();
+        this.getCryptoData();
     }
 
-    cryptoObject() {
-        return JSON.parse('[{"Bitcoin": "BTC","Ethereum": "ETH","Ripple": "XRP","Bitcoin Cash": "BCH","Cardano": "ADA","Litecoin": "LTC","Dash": "DASH","Monero": "XHR","Bitcoin Gold": "BTG","Ethereum Classic": "ETC","Zcash": "ZEC"}]');
-    }
-
-    objectToArray(thisObject: any): string[] {
-        return Object.keys(thisObject).map((k) => {
-            return thisObject[k]
-        });
-    }
-
-    getCrytpoData(): void {
-        let CryptoTypes = this.cryptoObject()
-        let cryptoArray = this.objectToArray(CryptoTypes[0]),
+    getCryptoData(): void {
+        const cryptoTypes: object[] = initialCoinList(),
+            cryptoTypesObject = cryptoTypes[0],
             cryptoList: string[] = [];
 
-        cryptoArray.map((j) => cryptoList.push(j));
+        for (const coin in cryptoTypesObject) { // @ts-ignore
+            cryptoList.push(cryptoTypesObject[coin]);
+        }
         const allCryptoList: string = cryptoList.join(",");
 
         fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${allCryptoList}&tsyms=ARS`)
-            .then((response) => {
-                return response.json()
-            })
+            .then((response) => { return response.json() })
 
-            .then((data) => {
-                console.log(data);
-                this.setState({
-                    crypto_result: data
-                })
-            })
+            .then((data) => { this.setState({ cryptoResult: data }) })
 
-            .catch((err) => {
-                console.error(`Fetch error: ${err}`)
-            })
+            .catch((err) => { console.error(`Fetch error: ${err}`) })
     }
 
     render() {
-        if (!this.state.crypto_result) {
+        if (!this.state.cryptoResult) {
             // Work on a network issue message here
             return null;
         } else {
@@ -74,7 +56,7 @@ class Crypto extends Component<cryptoProps, cryptoState> {
                                 <th>% Chng (24h)</th>
                             </tr>
                         </thead>
-                        <CryptoContent data = { this.state.crypto_result } />
+                        <CryptoContent data = { this.state.cryptoResult } />
                     </table>
                 </div>
             )
