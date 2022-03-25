31ef5ca990d17b7c0773c486af794c4d1eadd9de
Omeiza Owuda <owudaomeiza@gmail.com>
2022-03-25 12:56:49 +0100

    Design update

--- components/Crypto/CryptoTools.tsx
+++ components/Crypto/CryptoTools.tsx
@@ -1,17 +1,25 @@
+import Image from "next/image";
+
 const CryptoTools = (): JSX.Element => {
     return (
         <div className="crypto__tools">
             <div className="crypto__tools_alert">
-                <h2>Crypto Price Alert</h2>
-                <p>Get customized crypto alerts based on market price and volume sent to your email</p>
+                <div className="crypto__tools_img">
+                    <Image width = { 50 } height = { 50 } src="/img/crypto-alert-img.png" />
+                </div>
+                <div className="crypto__tools_content">
+                    <h2>Crypto Price Alert</h2>
+                    <p>Get customized crypto alerts based on market price and volume sent to your email</p>
+                </div>
             </div>
             <div className="crypto__tools_converter">
-                <h2>Crypto Converter</h2>
-                <p>Use our Cryptocurrency converter and calculator tool</p>
-            </div>
-            <div className="crypto__tools_info">
-                <h2>Crypto Newsletter</h2>
-                <p>Get updated about the Crypto world and how it's related to Africa</p>
+                <div className="crypto__tools_img">
+                    <Image width = { 50 } height = { 50 }  src="/img/crypto-converter-img.png" />
+                </div>
+                <div className="crypto__tools_content">
+                    <h2>Crypto Converter</h2>
+                    <p>Use our Cryptocurrency converter and calculator tool</p>
+                </div>
             </div>
         </div>
     )
